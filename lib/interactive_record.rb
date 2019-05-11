819d7330f71142ae3587a42939aa18fb5573b689
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-11 16:59:53 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -23,7 +23,7 @@ class InteractiveRecord
 
   def initialize(options={})
     options.each do |property, value|
-      self.send("#{property}=", value)e
+      self.send("#{property}=", value)
     end
   end
 
