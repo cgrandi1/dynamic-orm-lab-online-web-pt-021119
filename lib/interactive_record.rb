3e3b7068a9ef33c626b3eb534be7ed7866260e6b
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-11 17:51:04 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,9 +50,10 @@ class InteractiveRecord
     end
 
   def self.find_by_name(name)
-      binding.pry
+
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}' LIMIT 1"
     DB[:conn].execute(sql)
+    binding.pry
   end
 
   def self.find_by(name:, grade:)
