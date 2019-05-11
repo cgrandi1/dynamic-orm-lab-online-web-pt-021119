42397b187668908f7d9669f3806522d5da16f032
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-11 17:36:28 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(name, grade)
+  def self.find_by(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}', grade = '#{grade}'"
     DB[:conn].execute(sql)
   end
